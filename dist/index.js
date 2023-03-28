53bab8d8fbb9089b108549ef85a1e8107f707ff8
isa-ismail <fahimismail75.scc@gmail.com>
2023-03-28 12:49:20 +0600

    emo

--- dist/index.js
+++ dist/index.js
@@ -4,15 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const compression_1 = __importDefault(require("compression"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
+dotenv_1.default.config();
+const port = process.env.PORT || 5000;
+mongoose_1.default.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+app.use((0, cors_1.default)({
+    credentials: true,
+}));
+app.use(body_parser_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, compression_1.default)());
 app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+    return res.send('❤️😘🤣🤣🤣image.png');
 });
 app.get('/ping', (_req, res) => {
     return res.send('pong 🏓');
 });
 app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-  import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-  const app = express()
-  const port = process.env.PORT || 8080
+const app = express();
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+
+app.use(cors({
+    credentials: true,
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
 
   app.get('/', (_req: Request, res: Response) => {
-    return res.send('Express Typescript on Vercel')
+    return res.send('❤️😘🤣🤣🤣image.png')
   })
 
   app.get('/ping', (_req: Request, res: Response) => {
     return res.send('pong 🏓')
   })
 
-  app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`)
-  })
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
